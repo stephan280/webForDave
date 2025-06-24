4021abb5e6c7edf1140ff8731624af8338461c0a
David Akinola <dakinola54@gmail.com>
2025-06-24 17:24:52 +0100

    modified sprint 3

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,14 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
-from django.contrib.auth import views as auth_views
+from main import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('signup/', views.signup_view, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('paste/', views.paste_text, name='paste_text'),
     path('upload/', views.upload_pdf, name='upload_pdf'),
-    path('signup/', views.signup_view, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
